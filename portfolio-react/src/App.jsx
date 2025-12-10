e027501d8f9d7b77c8dd87c325bc78cd9146653a
Muntakim Ahmed Moon <alc.moon@hotmail.com>
2025-12-10 13:02:01 +0600

    commit reset
    b

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -18,7 +18,7 @@ const scriptSources = [
   '/igneous/js/fittext.min.js',
   '/igneous/js/addtohomescreen.min.js',
   '/igneous/js/spritespin.min.js',
-  '/igneous/js/script.min56b8.js?v=fast3',
+  '/igneous/js/script.min56b8.js?v=fast2',
 ]
 
 const loadScript = (src) =>
